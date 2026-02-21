f34a337de7cf86d41c04be846e57fb71174605bc
Van17-ux <gymnastiarjosivaneo@gmail.com>
2026-02-21 20:11:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -54,28 +54,20 @@ function renderMenu() {
         card.innerHTML = `
           <div class="menu-left">
           <div class="menu-img-wrapper">
-            ${item.badge ? `<div class="menu-badge">${item.badge}</div>` : ""}
-            <img
-              class="menu-img"
-              src="${item.image || ""}"
-              alt="${item.name}"
-              onclick="openProductModal(${item.id})"
-            />
-          </div>
-            <div class="menu-text">
-              <div class="menu-name">${item.name}</div>
-              <div class="menu-desc">${item.desc || ""}</div>
-              <div class="menu-price">Rp. ${safeMoney(item.price)}</div>
-            </div>
-          </div>
-
+          ${item.badge ? `<div class="menu-badge">${item.badge}</div>` : ""}
           ${item.available === false ? `<div class="sold-overlay">Sold Out</div>` : ""}
-
-          ${item.available === false 
-            ? `<button class="menu-add sold-out" disabled>Sold Out</button>`
-            : `<button class="menu-add" onclick="addToCart(${item.id})">Add</button>`
-          }
-        `;
+          <img
+            class="menu-img"
+            src="${item.image || ""}"
+            alt="${item.name}"
+            onclick="openProductModal(${item.id})"
+          />
+        </div>
+        
+        ${item.available === false
+          ? `<button class="menu-add sold-out" disabled>Sold Out</button>`
+          : `<button class="menu-add" onclick="addToCart(${item.id})">Add</button>`
+        }
 
         menuEl.appendChild(card);
       });
@@ -356,6 +348,7 @@ Total: Rp. ${safeMoney(window.lastOrderData.total)}`;
   const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
   window.open(url, "_blank");
 });
+
 
 
 
